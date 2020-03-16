c7232318dedd49808d0bf17cab7713bd0e9fcc65
Fernando Basso <fernandobasso.br@gmail.com>
2020-03-16 16:29:21 -0300

    Add basic jest config with coverage threshold

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+
+//
+// https://jestjs.io/docs/en/configuration#coveragethreshold-object
+//
+module.exports = {
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -6
+    }
+  }
+};
+
